26352a744a86a80c54d8056e708dc83b96e33ef5
Foster Stule <camodude5@yahoo.com>
2018-05-25 11:49:30 -0400

    made the testing more stream line

--- ifelsetesting with states.sql
+++ ifelsetesting with states.sql
@@ -1,6 +1,6 @@
 declare @statecode nvarchar(2);
 
-set @statecode = 'OH';
+set @statecode = 'ky';
 
 declare @op1 int;
 declare @op2 int;
@@ -9,14 +9,20 @@ if @statecode='OH'
 begin
 	set @op1=1000;
 	set @op2=500;
+
 end
 else if @statecode='KY'
 begin
 	set @op1=900;
 	set @op2=400;
+		
 end
 else if @statecode='IN'
 begin 
 	set @op1=950;
-	set @op2=450
+	set @op2=450;
+
 end
+	select * from [order] o
+		join Customer c on o.CustomerId=c.Id
+		where c.State= @statecode and o.Amount>@op2 and o.Amount<@op1
